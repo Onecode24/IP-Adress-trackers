2271f1eacc45940f6afbdb8195abd33d7392b275
Onecode24 <88493737+Onecode24@users.noreply.github.com>
2021-11-17 12:07:59 +0100

    five commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,76 +6,21 @@ let locations=document.querySelector('#location')
 let timezone=document.querySelector('#timezone')
 let isp=document.querySelector("#isp")
 let longitude,latitude,ips;
-var map = L.map('map').setView([ 6.3561, 2.3833], 17);
-let maps=document.querySelector('#map')
+let maps=document.querySelector('.container')
 let api_key = 'at_tUakp5N6aY0wpTauakIS5I7ewNV3l'
 
 //initialisation de la maps
+var map = L.map('map').setView([ 6.3561, 2.3833], 17);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     L.marker([6.3561, 2.3833]).addTo(map)
         .bindPopup(`Benin Airport is here`)
-        .openPopup();       
-
-/*
-//Au chargement de la page on a
-window.addEventListener('pageshow',()=>{
-    fetch("https://api64.ipify.org?format=json")
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        newIP=data.ip;
-        search.value=newIP;
-      fetch(`http://ip-api.com/json/${newIP}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,query`)
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        infos=data;
-         longitude=infos.lon;
-         latitude=infos.lat;
-        ip.innerHTML=`${infos.query}`;
-        locations.innerHTML=`${infos.city},${infos.country}`
-        timezone.innerHTML=`UTC-${infos.continent}`
-        isp.innerHTML=`${infos.isp}`
-        map.setView([latitude, longitude], 16)
-        L.marker([latitude, longitude]).addTo(map)
-        .bindPopup(`${infos.query} is here`)
-        .openPopup();       
-    }) 
-    
-       
-    })
-    
-})
-//Au renseignement d'une nouvelle adresses ip
-submit.addEventListener('click',()=>{
-    newIP=search.value;
-  fetch(`http://ip-api.com/json/${newIP}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,query`)
-  .then((response)=>{
-      return response.json();
-  })
-  .then((data)=>{
-      infos=data;
-       longitude=infos.lon;
-       latitude=infos.lat ;
-       ip.innerHTML=`${infos.query}`;
-        locations.innerHTML=`${infos.city},${infos.country}`
-        timezone.innerHTML=`UTC-${infos.continent}`
-        isp.innerHTML=`${infos.isp}`
-        map.setView([latitude,longitude], 16)
-        L.marker([latitude, longitude]).addTo(map)
-        .bindPopup(`${infos.query} is here`)
-        .openPopup();
-    })
-        
-
-})
- */
+        .openPopup();     
 
 
+
+//Au chargement de la page 
 
 window.addEventListener('pageshow',()=>{
     fetch("https://api64.ipify.org?format=json")
@@ -98,18 +43,23 @@ window.addEventListener('pageshow',()=>{
             locations.innerHTML=`${infos.location.region},${infos.location.city}`
             timezone.innerHTML=`UTC${infos.location.timezone}`
             isp.innerHTML=`${infos.as.name}`
-            map.setView([latitude, longitude], 17)
-            L.marker([latitude, longitude]).addTo(map)
-            .bindPopup(`${infos.ip} is here`)
-            .openPopup();
-            maps.style.visibility = 'visible'
+                //Actualisation de la maps pour le client
+            map.setView([ longitude,latitude], 17);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+                L.marker([longitude,latitude]).addTo(map)
+                    .bindPopup(`${infos.ip} is here`)
+                    .openPopup(); 
+
         })
     })
 });
 
+//A l'entrez d'une nouvelle adress ip
+
 submit.addEventListener('click',()=>{
     newIP=search.value;
-  //fetch(`http://ip-api.com/json/${newIP}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,query`)
   fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${newIP}`)
   .then((response)=>{
       return response.json();
@@ -122,8 +72,12 @@ submit.addEventListener('click',()=>{
       locations.innerHTML=`${infos.location.region},${infos.location.city}`
       timezone.innerHTML=`UTC${infos.location.timezone}`
       isp.innerHTML=`${infos.as.domain}`
-      map.setView([latitude, longitude], 17)
-      L.marker([latitude, longitude]).addTo(map)
+            //Actualisation de la maps pour l'adress Ip saisir par le client
+      map.setView([longitude,latitude], 17)
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+      L.marker([longitude,latitude]).addTo(map)
       .bindPopup(`${infos.ip} is here`)
       .openPopup();
       map.style.display= 'none'
